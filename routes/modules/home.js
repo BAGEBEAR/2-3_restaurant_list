9711afca3423a069b43befc6554ac1684268aa32
bagebear <bagebear@gmail.com>
2021-08-28 02:29:29 +0800

    feat: fix js standard style

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,9 +6,9 @@ const Restaurant = require('../../models/restaurant')
 router.get('/', (req, res) => {
   const keyword = req.query.keyword
   const keywordRegex = new RegExp(keyword, 'i')
-    
+  
   Restaurant.find({
-    $or: [{ name: { $regex: keywordRegex }}, { category: { $regex: keywordRegex }}]
+    $or: [{ name: { $regex: keywordRegex } }, { category: { $regex: keywordRegex } }]
   })
     .lean()
     .then(restaurants => res.render('index', { restaurants, keyword }))
